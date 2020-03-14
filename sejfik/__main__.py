abd038c6cef9d57fb21f291deb7ba06c695cd1a3
amfu05 <1618jankowalski@gmail.com>
2020-03-14 12:46:03 +0100

    Added basic logs.

--- sejfik/__main__.py
+++ sejfik/__main__.py
@@ -40,59 +40,81 @@ class Sejfik:
         self.password = password
         self.page_delay = page_delay
 
-        if show_logs:
-            pass
-
         if disable_image_load:
             chrome_prefs['images': 2]
 
-        if platform == 'linux' or platform == 'linux2':
-            pass
-
-        elif platform == 'darwin':
-            pass
-
-        elif platform == 'win32':
-            pass
-
-        def get_sejfik_logger(self, show_logs: bool):
+        def set_logger(self, show_logs: bool):
             """Handles the creation of logger"""
+
+            if show_logs:
+                logging.basicConfig(
+                    filename=logs_path,
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+                
+            logging.debug('Set up logger.')
+
             return self
 
         def find_chromedriver(self):
             """Finds chromedriver binaries"""
+
             return self
 
         def download_chromedriver(self):
             """Downloads chromedriver binaries"""
+
             return self
 
         def set_selenium_session(self, logger):
-            """Starts selenium session"""
+            """Sets selenium session"""
+
+            if platform == 'linux' or platform == 'linux2':
+                logging.debug('Linux has been detected.')
+
+            elif platform == 'darwin':
+                logging.debug('Mac OS has been detected.')
+
+            elif platform == 'win32':
+                logging.debug('Windows has been detected.')
+
+            logging.debug('Selenium session has been started.')
+
             return self
-            
+
         def set_webui_session(self):
-            """Starts web interface session on localhost"""
+            """Sets web interface session on localhost"""
+
+            logging.debug('WebUI session has been started.')
+
             return self
-            
 
         def login(self):
             """Login the user either with the username and password"""
+
+            logging.debug('Logged in.')
+
             return self
-            
-            
+
         def get_ptc_links(self):
             """Scrapes pay to click links"""
+
+            logging.debug('Scrapped pay to click links.')
+
             return self
-            
-            
+
         def get_inbox_links(self):
             """Scrapes inbox links"""
+
+            logging.debug('Scrapped inbox links.')
+
             return self
-            
-            
+
         def get_startpage_link(self):
             """Scrapes and cache starting page link"""
+
+            logging.debug('Scrapped start page link.')
+
             return self
-            
-            
