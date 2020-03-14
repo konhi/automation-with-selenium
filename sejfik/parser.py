4b1faf707fe7e9b3a8d3538c2a1cabf537176521
amfu05 <1618jankowalski@gmail.com>
2020-03-14 18:46:24 +0100

    Make very basic config file creator.

--- sejfik/parser.py
+++ sejfik/parser.py
@@ -1,1 +1,11 @@
 import configparser
+import os
+
+config_dir = os.path.dirname(os.path.abspath(__file__)) + '/config'
+
+if not os.listdir(config_dir):
+    print("Didn't found any config files. Creating one...")
+    os.chdir(config_dir)
+
+    with open(input('Enter username: ') + '.cfg'):
+        pass
