74e92cef560810d42583b6f91d6f390310700740
amfu05 <1618jankowalski@gmail.com>
2020-03-15 16:55:06 +0100

    Add setup & configs

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Sejfik',
+      version='0.1',
+      description='Tool for automated sejfik tasks.',
+      author='amfu05',
+      packages=['sejfik'],
+      )
